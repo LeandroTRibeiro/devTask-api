d290883c306cd0adb682b8d94a6e82aad109fed7
LeandroTRibeiro <leandrothiago_ribeiro@hotmail.com>
2023-05-24 09:08:01 -0300

    tests

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -8,11 +8,10 @@ mongoose.set('strictQuery', true);
 export const mongodbConnect = async () => {
     try {
         await connect(process.env.DATABASE_URL as string);
-        console.log('{ mongodb: true }');
+        if(!process.env.NODE_ENV) console.log('{ mongodb: true }');
     } catch(error) {
-        console.log('{mongodb : false}', error);
+        if(!process.env.NODE_ENV) console.log('{mongodb : false}', error);
     };
 };
-
 
 
